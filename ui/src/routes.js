f20d92a88449cea3eeaa82b355e1656bcaf0aaf3
Francis Anyuru <anyuruf@gmail.com>
2025-10-15 23:51:01 -0400

    delete unnecessary jsconfig file

--- ui/src/routes.js
+++
@@ -1,9 +0,0 @@
-import {
-  route,
-} from "@react-router/dev/routes";
-
-export default [
-  // * matches all URLs, the ? makes it optional so it will match / as well
-   route("/", "./pages/LandingPage/HomePage.jsx"),
-  route("*?", "catchall.jsx"),
-];
