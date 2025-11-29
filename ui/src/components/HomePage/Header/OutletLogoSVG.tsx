8cca39be643360b231b2466e3c7a101c92a8a206
Francis Anyuru <anyuruf@gmail.com>
2025-11-29 07:49:12 -0500

    add application shell and design new header

--- ui/src/components/HomePage/Header/OutletLogoSVG.tsx
+++ ui/src/components/HomePage/Header/OutletLogoSVG.tsx
@@ -1,191 +1,342 @@
-import React from "react";
-
-export default  function OutletLogoSVG(props: React.SVGAttributes<SVGElement>) {
-  return (
-    <svg
-      width="289.16516"
-      height="104.92855"
-      viewBox="0 0 289.16516 104.92855"
-      version="1.1"
-      id="svg1"
-      xml:space="preserve"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:svg="http://www.w3.org/2000/svg"><defs
-     id="defs1"><linearGradient
-       id="linearGradient6"><stop
-         style="stop-color:#990202;stop-opacity:1;"
-         offset="0"
-         id="stop7" /><stop
-      style="stop-color:#fc8708;stop-opacity:0.99088544;"
-      offset="1"
-      id="stop8" /></linearGradient><linearGradient
-      id="linearGradient19"><stop
-         style="stop-color:#e38c02;stop-opacity:1;"
-         offset="0"
-         id="stop20" /><stop
-      style="stop-color:#a93d07;stop-opacity:0.95388192;"
-      offset="1"
-      id="stop21" /></linearGradient><linearGradient
-      id="linearGradient18"><stop
-         style="stop-color:#590b02;stop-opacity:0.95815313;"
-         offset="0"
-         id="stop18" /><stop
-      style="stop-color:#a60c04;stop-opacity:0.9786846;"
-      offset="0.98315376"
-      id="stop19" /></linearGradient><linearGradient
-      id="swatch10"><stop
-         style="stop-color:#8c0101;stop-opacity:0.96516484;"
-         offset="0"
-         id="stop11" /></linearGradient><rect
-      x="45.893223"
-      y="119.9692"
-      width="177.30214"
-      height="66.439491"
-      id="rect1" /><radialGradient
-      xlink:href="#linearGradient18"
-      id="radialGradient19"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.089061"
-      gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
-      gradientUnits="userSpaceOnUse" /><radialGradient
-      xlink:href="#linearGradient19"
-      id="radialGradient21"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.752971"
-      gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
-      gradientUnits="userSpaceOnUse" /><radialGradient
-      xlink:href="#linearGradient19"
-      id="radialGradient3"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.752971" /><radialGradient
-      xlink:href="#linearGradient18"
-      id="radialGradient4"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.089061" /><radialGradient
-      xlink:href="#linearGradient19"
-      id="radialGradient5"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.752971" /><radialGradient
-      xlink:href="#linearGradient18"
-      id="radialGradient6"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.089061" /><radialGradient
-      xlink:href="#linearGradient19"
-      id="radialGradient7"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.752971" /><radialGradient
-      xlink:href="#linearGradient18"
-      id="radialGradient8"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.089061" /><radialGradient
-      xlink:href="#linearGradient19"
-      id="radialGradient9"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.752971" /><radialGradient
-      xlink:href="#linearGradient18"
-      id="radialGradient10"
-      gradientUnits="userSpaceOnUse"
-      gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
-      cx="35.966846"
-      cy="140.17133"
-      fx="35.966846"
-      fy="140.17133"
-      r="13.089061" /><linearGradient
-      xlink:href="#linearGradient6"
-      id="linearGradient10"
-      gradientUnits="userSpaceOnUse"
-      x1="48.768414"
-      y1="162.79819"
-      x2="220.27667"
-      y2="135.63396" /><linearGradient
-      xlink:href="#swatch10"
-      id="linearGradient11"
-      gradientUnits="userSpaceOnUse"
-      x1="47.699509"
-      y1="149.21603"
-      x2="221.34555"
-      y2="149.21603" /></defs><g
-      id="layer1"
-      transform="translate(-1.8695695,-97.535729)"><g
-       id="g3"
-       transform="matrix(3.2087866,0,0,3.7117156,-51.986436,-363.30623)"><g
-         id="g2"
-         style="fill:#de4f07;fill-opacity:1;stroke-width:0.99997;stroke-dasharray:none"><g
-           id="g5"
-           transform="translate(-5.4299578,-1.8776844)"><g
-             id="g4"><g
-               id="g9"><path
-                 d="m 47,141 h 0.01"
-                 id="path3-2"
-                 transform="translate(5.4299578,1.8776844)" /><text
-      xml:space="preserve"
-      id="text1"
-      style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-family:'Noto Sans Adlam Unjoined';-inkscape-font-specification:'Noto Sans Adlam Unjoined Bold';text-align:start;writing-mode:lr-tb;direction:ltr;white-space:pre;shape-inside:url(#rect1);shape-padding:0;display:inline;fill:url(#linearGradient10);fill-opacity:1;stroke:url(#linearGradient11);stroke-width:3.0532;stroke-dasharray:none"
-      transform="matrix(0.3200824,0,0,0.33512207,41.432618,90.16562)"><tspan
-                   x="45.892578"
-                   y="168.87337"
-                   id="tspan6"><tspan
-                     style="font-size:52.9134px;font-family:MathJax_Caligraphic;-inkscape-font-specification:'MathJax_Caligraphic, Bold'"
-                     id="tspan2">O</tspan><tspan
-      style="font-size:52.9134px;font-family:MathJax_Caligraphic;-inkscape-font-specification:'MathJax_Caligraphic, Bold'"
-      id="tspan5">utlet</tspan></tspan></text><g
-      id="g17"
-      transform="translate(0,-1.1260316e-4)"
-      style="fill:url(#radialGradient21);stroke:url(#radialGradient19)"><path
-                   d="M 40.202465,153.64219 A 3.926718,4.0412579 0 0 0 37.32287,146.90674"
-                   id="path1"
-                   style="fill:url(#radialGradient3);stroke:url(#radialGradient4);stroke-width:1.32782" /><path
-      d="m 41.202468,150.94799 a 3.926718,4.0412579 0 0 1 3.926719,-4.04125 h 1.308905 a 2.617812,2.694172 0 0 0 2.617814,-2.69415 v -2.6942 a 5.2356238,5.388344 0 0 0 -5.235628,-5.38835 v -6.7354"
-      id="path2"
-      style="fill:url(#radialGradient5);stroke:url(#radialGradient6);stroke-width:1.32782" /><path
-      d="m 43.820278,141.51839 h 0.01302"
-      id="path3"
-      style="fill:url(#radialGradient7);stroke:url(#radialGradient8);stroke-width:1.32782" /><path
-      d="m 43.820278,132.08879 a 5.2356238,5.388344 0 0 0 -5.235623,5.38835 9.162342,9.429602 0 0 0 -9.162342,9.4296 c 0,-6.7354 5.235627,-6.7354 5.235627,-14.1444 a 5.890078,6.0618881 0 1 0 -11.780156,0 3.272265,3.3677149 0 0 0 6.544529,0 c 0,4.7148 -5.235624,6.0619 -5.235624,14.1444 0,3.77185 2.879596,6.73545 6.544533,6.73545 h 13.089056"
-      id="path4"
-      style="fill:url(#radialGradient9);stroke:url(#radialGradient10);stroke-width:1.32782" /></g></g></g></g></g></g></g>
-    </svg>
-  );
-};
+import * as React from "react";
+import { CSSProperties } from "react";
+const OutletLogoSVG = (props) => (
+  <svg
+    width={289.16516}
+    height={104.92855}
+    viewBox="0 0 289.16516 104.92855"
+    id="svg1"
+    xmlSpace="preserve"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:svg="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <defs id="defs1">
+      <linearGradient id="linearGradient6">
+        <stop
+          style={{
+            stopColor: "#990202",
+            stopOpacity: 1,
+          }}
+          offset={0}
+          id="stop7"
+        />
+        <stop
+          style={{
+            stopColor: "#fc8708",
+            stopOpacity: 0.99088544,
+          }}
+          offset={1}
+          id="stop8"
+        />
+      </linearGradient>
+      <linearGradient id="linearGradient19">
+        <stop
+          style={{
+            stopColor: "#e38c02",
+            stopOpacity: 1,
+          }}
+          offset={0}
+          id="stop20"
+        />
+        <stop
+          style={{
+            stopColor: "#a93d07",
+            stopOpacity: 0.95388192,
+          }}
+          offset={1}
+          id="stop21"
+        />
+      </linearGradient>
+      <linearGradient id="linearGradient18">
+        <stop
+          style={{
+            stopColor: "#590b02",
+            stopOpacity: 0.95815313,
+          }}
+          offset={0}
+          id="stop18"
+        />
+        <stop
+          style={{
+            stopColor: "#a60c04",
+            stopOpacity: 0.9786846,
+          }}
+          offset={0.98315376}
+          id="stop19"
+        />
+      </linearGradient>
+      <linearGradient id="swatch10">
+        <stop
+          style={{
+            stopColor: "#8c0101",
+            stopOpacity: 0.96516484,
+          }}
+          offset={0}
+          id="stop11"
+        />
+      </linearGradient>
+      <rect
+        x={45.893223}
+        y={119.9692}
+        width={177.30214}
+        height={66.439491}
+        id="rect1"
+      />
+      <radialGradient
+        xlinkHref="#linearGradient18"
+        id="radialGradient19"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.089061}
+        gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <radialGradient
+        xlinkHref="#linearGradient19"
+        id="radialGradient21"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.752971}
+        gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
+        gradientUnits="userSpaceOnUse"
+      />
+      <radialGradient
+        xlinkHref="#linearGradient19"
+        id="radialGradient3"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.752971}
+      />
+      <radialGradient
+        xlinkHref="#linearGradient18"
+        id="radialGradient4"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.089061}
+      />
+      <radialGradient
+        xlinkHref="#linearGradient19"
+        id="radialGradient5"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.752971}
+      />
+      <radialGradient
+        xlinkHref="#linearGradient18"
+        id="radialGradient6"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.089061}
+      />
+      <radialGradient
+        xlinkHref="#linearGradient19"
+        id="radialGradient7"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.752971}
+      />
+      <radialGradient
+        xlinkHref="#linearGradient18"
+        id="radialGradient8"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.089061}
+      />
+      <radialGradient
+        xlinkHref="#linearGradient19"
+        id="radialGradient9"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.0277618,0,-3.8914037)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.752971}
+      />
+      <radialGradient
+        xlinkHref="#linearGradient18"
+        id="radialGradient10"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(1,0,0,1.02917,0,-4.0887995)"
+        cx={35.966846}
+        cy={140.17133}
+        fx={35.966846}
+        fy={140.17133}
+        r={13.089061}
+      />
+      <linearGradient
+        xlinkHref="#linearGradient6"
+        id="linearGradient10"
+        gradientUnits="userSpaceOnUse"
+        x1={48.768414}
+        y1={162.79819}
+        x2={220.27667}
+        y2={135.63396}
+      />
+      <linearGradient
+        xlinkHref="#swatch10"
+        id="linearGradient11"
+        gradientUnits="userSpaceOnUse"
+        x1={47.699509}
+        y1={149.21603}
+        x2={221.34555}
+        y2={149.21603}
+      />
+    </defs>
+    <g id="layer1" transform="translate(-1.8695695,-97.535729)">
+      <g
+        id="g3"
+        transform="matrix(3.2087866,0,0,3.7117156,-51.986436,-363.30623)"
+      >
+        <g
+          id="g2"
+          style={{
+            fill: "#de4f07",
+            fillOpacity: 1,
+            strokeWidth: 0.99997,
+            strokeDasharray: "none",
+          }}
+        >
+          <g id="g5" transform="translate(-5.4299578,-1.8776844)">
+            <g id="g4">
+              <g id="g9">
+                <path
+                  d="m 47,141 h 0.01"
+                  id="path3-2"
+                  transform="translate(5.4299578,1.8776844)"
+                />
+                <text
+                  xmlSpace="preserve"
+                  id="text1"
+                  style={{
+                    fontStyle: "normal",
+                    fontVariant: "normal",
+                    fontWeight: "bold",
+                    fontStretch: "normal",
+                    fontFamily: "'Noto Sans Adlam Unjoined'",
+                    InkscapeFontSpecification:
+                      "'Noto Sans Adlam Unjoined Bold'",
+                    textAlign: "start",
+                    writingMode: "lr-tb",
+                    direction: "ltr",
+                    whiteSpace: "pre",
+                    shapeInside: "url(#rect1)",
+                    shapePadding: 0,
+                    display: "inline",
+                    fill: "url(#linearGradient10)",
+                    fillOpacity: 1,
+                    stroke: "url(#linearGradient11)",
+                    strokeWidth: 3.0532,
+                    strokeDasharray: "none",
+                  } as CSSProperties}
+                  transform="matrix(0.3200824,0,0,0.33512207,41.432618,90.16562)"
+                >
+                  <tspan x={45.892578} y={168.87337} id="tspan6">
+                    <tspan
+                      style={{
+                        fontSize: "52.9134px",
+                        fontFamily: "MathJax_Caligraphic",
+                        InkscapeFontSpecification:
+                          "'MathJax_Caligraphic, Bold'",
+                      }}
+                      id="tspan2"
+                    >
+                      {"O"}
+                    </tspan>
+                    <tspan
+                      style={{
+                        fontSize: "52.9134px",
+                        fontFamily: "MathJax_Caligraphic",
+                        InkscapeFontSpecification:
+                          "'MathJax_Caligraphic, Bold'",
+                      }}
+                      id="tspan5"
+                    >
+                      {"utlet"}
+                    </tspan>
+                  </tspan>
+                </text>
+                <g
+                  id="g17"
+                  transform="translate(0,-1.1260316e-4)"
+                  style={{
+                    fill: "url(#radialGradient21)",
+                    stroke: "url(#radialGradient19)",
+                  }}
+                >
+                  <path
+                    d="M 40.202465,153.64219 A 3.926718,4.0412579 0 0 0 37.32287,146.90674"
+                    id="path1"
+                    style={{
+                      fill: "url(#radialGradient3)",
+                      stroke: "url(#radialGradient4)",
+                      strokeWidth: 1.32782,
+                    }}
+                  />
+                  <path
+                    d="m 41.202468,150.94799 a 3.926718,4.0412579 0 0 1 3.926719,-4.04125 h 1.308905 a 2.617812,2.694172 0 0 0 2.617814,-2.69415 v -2.6942 a 5.2356238,5.388344 0 0 0 -5.235628,-5.38835 v -6.7354"
+                    id="path2"
+                    style={{
+                      fill: "url(#radialGradient5)",
+                      stroke: "url(#radialGradient6)",
+                      strokeWidth: 1.32782,
+                    }}
+                  />
+                  <path
+                    d="m 43.820278,141.51839 h 0.01302"
+                    id="path3"
+                    style={{
+                      fill: "url(#radialGradient7)",
+                      stroke: "url(#radialGradient8)",
+                      strokeWidth: 1.32782,
+                    }}
+                  />
+                  <path
+                    d="m 43.820278,132.08879 a 5.2356238,5.388344 0 0 0 -5.235623,5.38835 9.162342,9.429602 0 0 0 -9.162342,9.4296 c 0,-6.7354 5.235627,-6.7354 5.235627,-14.1444 a 5.890078,6.0618881 0 1 0 -11.780156,0 3.272265,3.3677149 0 0 0 6.544529,0 c 0,4.7148 -5.235624,6.0619 -5.235624,14.1444 0,3.77185 2.879596,6.73545 6.544533,6.73545 h 13.089056"
+                    id="path4"
+                    style={{
+                      fill: "url(#radialGradient9)",
+                      stroke: "url(#radialGradient10)",
+                      strokeWidth: 1.32782,
+                    }}
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default OutletLogoSVG;
--- ui/src/components/RootLayout/AppHeader/NotificationMenu.tsx
+++ ui/src/components/RootLayout/AppHeader/NotificationMenu.tsx
@@ -8,6 +8,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import { BellIcon } from "lucide-react";
 
 export const NotificationMenu = ({
